eb91843323e2fcfdca2cba273f54e1623d089131
Irina Selivanova <irina.selivanova1106@gmail.com>
2021-05-30 15:12:02 +0300

    init: start auto-complite-ui task

---
+++ auto-complete-ui/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require('webpack-merge');
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index.ts'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+    ],
+};
+
+module.exports = ({mode}) => {
+    const isProd = mode === 'prod';
+    const envConfig = isProd
+        ? require('./webpack.prod.config')
+        : require('./webpack.dev.config');
+    return merge(baseConfig, envConfig);
+}
